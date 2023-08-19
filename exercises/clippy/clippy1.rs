d81286040b6c01f971a9f2403145bdec2d05428b
hzh9810 <1528817504@qq.com>
2023-08-19 17:18:16 +0800

    Initial commit

--- exercises/clippy/clippy1.rs
+++ exercises/clippy/clippy1.rs
@@ -14,10 +14,10 @@
 use std::f32;
 
 fn main() {
-    let pi = 3.14f32;
+    let pi = f32::consts::PI;
     let radius = 5.00f32;
 
-    let area = pi * f32::radius.powi(2);
+    let area = pi * f32::powi(radius,2);
 
     println!(
         "The area of a circle with radius {:.2} is {:.5}!",
--- exercises/enums/enums3.rs
+++ exercises/enums/enums3.rs
@@ -5,10 +5,14 @@
 // Execute `rustlings hint enums3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+
 
 enum Message {
     // TODO: implement the message variant types based on their usage below
+    Move(Point),
+    Echo(String),
+    ChangeColor(u8, u8, u8),
+    Quit,
 }
 
 struct Point {
@@ -43,6 +47,12 @@ impl State {
         // variants
         // Remember: When passing a tuple as a function argument, you'll need
         // extra parentheses: fn function((t, u, p, l, e))
+        match message{
+            Message::Move(v) => self.move_position(v),
+            Message::Echo(v) => self.echo(v),
+            Message::ChangeColor(v1,v2,v3) => self.change_color((v1,v2,v3)),
+            Message::Quit => self.quit(),
+        }
     }
 }
 
